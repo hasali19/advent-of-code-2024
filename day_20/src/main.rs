2136824ad82837107d37a935322e119e1b139b64
Hasan Mohsin <hasan@hasali.dev>
2024-12-23 13:27:13 +0000

    Day 20 part 2

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,12 +1,11 @@
 #![feature(gen_blocks, let_chains)]
 
-use std::collections::{HashMap, HashSet};
-use std::rc::Rc;
+use std::collections::HashMap;
 
 use aoc2024::aoc_solution;
 use eyre::bail;
 use itertools::{Itertools, iproduct};
-use pathfinding::prelude::dfs;
+use pathfinding::prelude::{bfs_reach, dfs};
 
 fn main() -> eyre::Result<()> {
     aoc_solution(20, |input| {
@@ -16,12 +15,73 @@ fn main() -> eyre::Result<()> {
     })
 }
 
+type Grid = Vec<Vec<char>>;
+
 fn part_1(input: &str) -> eyre::Result<()> {
     let grid = input
         .lines()
         .map(|line| line.chars().collect_vec())
         .collect_vec();
 
+    let cheats = cheats(&grid, 2)?;
+
+    let count = cheats.filter(|(_, _, d)| *d >= 100).count();
+
+    println!("{count}");
+
+    Ok(())
+}
+
+fn part_2(input: &str) -> eyre::Result<()> {
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    let cheats = cheats(&grid, 20)?;
+
+    let count = cheats.filter(|(_, _, d)| *d >= 100).count();
+
+    println!("{count}");
+
+    Ok(())
+}
+
+type Cheat = ((usize, usize), (usize, usize), usize);
+
+/// Finds all cheats in the grid with a given max length.
+fn cheats(grid: &Grid, max_len: usize) -> eyre::Result<impl Iterator<Item = Cheat>> {
+    let path = find_path(grid)?;
+
+    // Build a map of the distances to the end from each node along the path.
+    let dists = path
+        .iter()
+        .rev()
+        .copied()
+        .enumerate()
+        .map(|(i, n)| (n, i))
+        .collect::<HashMap<_, _>>();
+
+    // For each node along the path, find all cheats starting at that node and calculate how much
+    // time each cheat will save.
+    let cheats = path
+        .into_iter()
+        .flat_map(move |(ax, ay)| cheats_from(grid, ax, ay, max_len).map(move |b| ((ax, ay), b)))
+        .filter_map(move |(a, (b, d))| {
+            let a_dist = dists.get(&a).unwrap();
+            let b_dist = dists.get(&b).unwrap();
+            if *a_dist > *b_dist + d {
+                Some((a, b, a_dist - (b_dist + d)))
+            } else {
+                None
+            }
+        });
+
+    Ok(cheats)
+}
+
+/// Computes the path from the start 'S' to the end 'E' in `grid`.
+fn find_path(grid: &Grid) -> eyre::Result<Vec<(usize, usize)>> {
     let width = grid[0].len();
     let height = grid.len();
 
@@ -33,11 +93,7 @@ fn part_1(input: &str) -> eyre::Result<()> {
         .find(|&(x, y)| grid[y][x] == 'E')
         .unwrap();
 
-    fn successors(
-        grid: &Vec<Vec<char>>,
-        x: usize,
-        y: usize,
-    ) -> impl Iterator<Item = (usize, usize)> {
+    fn successors(grid: &Grid, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
         let width = grid[0].len();
         let height = grid.len();
         gen move {
@@ -53,60 +109,42 @@ fn part_1(input: &str) -> eyre::Result<()> {
         }
     }
 
-    let Some(path) = dfs(start, |&(x, y)| successors(&grid, x, y), |&n| n == end) else {
+    let Some(path) = dfs(start, |&(x, y)| successors(grid, x, y), |&n| n == end) else {
         bail!("failed to find a path from {start:?} to {end:?}");
     };
 
-    let dists = path
-        .iter()
-        .copied()
-        .enumerate()
-        .map(|(i, n)| (n, path.len() - i - 1))
-        .collect::<HashMap<_, _>>();
-
-    fn cheats(grid: &Vec<Vec<char>>, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
-        let width = grid[0].len();
-        let height = grid.len();
-        gen move {
-            for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-                if let (Some(x), Some(y)) = (x.checked_add_signed(dx), y.checked_add_signed(dy))
-                    && x < width
-                    && y < height
-                    && grid[y][x] == '#'
-                    && let (Some(x), Some(y)) = (x.checked_add_signed(dx), y.checked_add_signed(dy))
-                    && x < width
-                    && y < height
-                    && grid[y][x] != '#'
-                {
-                    yield (x, y);
-                }
-            }
-        }
-    }
-
-    let cheats = path
-        .iter()
-        .copied()
-        .flat_map(|(ax, ay)| cheats(&grid, ax, ay).map(move |b| ((ax, ay), b)))
-        .unique()
-        .filter_map(|(a, b)| {
-            let a_dist = dists.get(&a).unwrap();
-            let b_dist = dists.get(&b).unwrap();
-            if *a_dist > *b_dist + 2 {
-                Some((a, b, a_dist - (b_dist + 2)))
-            } else {
-                None
-            }
-        })
-        .collect_vec();
-
-    let count = cheats.iter().filter(|(_, _, d)| *d >= 100).count();
-
-    println!("{count}");
-
-    Ok(())
+    Ok(path)
 }
 
-fn part_2(input: &str) -> eyre::Result<()> {
-    Ok(())
+/// Find all cheats with the given `max_len` starting at `(x, y)`.
+fn cheats_from(
+    grid: &Grid,
+    x: usize,
+    y: usize,
+    max_len: usize,
+) -> impl Iterator<Item = ((usize, usize), usize)> {
+    let width = grid[0].len();
+    let height = grid.len();
+
+    let successors = |&((x, y), d): &((usize, usize), usize)| gen move {
+        if d == max_len {
+            return;
+        }
+
+        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            if let (Some(x), Some(y)) = (x.checked_add_signed(dx), y.checked_add_signed(dy))
+                && x < width
+                && y < height
+            {
+                yield ((x, y), d + 1);
+            }
+        }
+    };
+
+    // BFS to find all reachable nodes from (x, y), up to a maximum distance `max_len`.
+    bfs_reach(((x, y), 0), successors)
+        .filter(|&((x, y), _)| grid[y][x] != '#')
+        .into_grouping_map()
+        .min() // when there are multiple paths to a node we take the shortest
+        .into_iter()
 }
