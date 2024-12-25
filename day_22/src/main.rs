5e1bf5856962e1abd38460e6f63688ae3347bc4f
Hasan Mohsin <hasan@hasali.dev>
2024-12-25 20:50:36 +0000

    Day 22 part 2

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,4 +1,10 @@
+#![feature(gen_blocks)]
+
+use std::collections::{HashMap, HashSet};
+use std::iter;
+
 use aoc2024::aoc_solution;
+use itertools::Itertools;
 
 fn main() -> eyre::Result<()> {
     aoc_solution(22, |input| {
@@ -12,11 +18,7 @@ fn part_1(input: &str) -> eyre::Result<()> {
     let mut sum = 0;
 
     for n in input.lines() {
-        let mut n = n.parse::<usize>()?;
-        for _ in 0..2000 {
-            n = next(n);
-        }
-        sum += n;
+        sum += secret_numbers(n.parse()?).take(2000).last().unwrap();
     }
 
     println!("{sum}");
@@ -25,7 +27,46 @@ fn part_1(input: &str) -> eyre::Result<()> {
 }
 
 fn part_2(input: &str) -> eyre::Result<()> {
+    let mut counts = HashMap::new();
+
+    for n in input.lines() {
+        let n = n.parse()?;
+
+        let prices = iter::once(n)
+            .chain(secret_numbers(n).take(2000))
+            .map(|n| n % 10)
+            .collect_vec();
+
+        let mut sequences = HashSet::new();
+        for (i, (a, b, c, d)) in price_changes(&prices).tuple_windows().enumerate() {
+            if sequences.insert((a, b, c, d)) {
+                *counts.entry((a, b, c, d)).or_insert(0) += prices[i + 4];
+            }
+        }
+    }
+
+    let max = counts.values().copied().max();
+
+    println!("{:?}", max);
+
     Ok(())
+}
+
+fn secret_numbers(mut n: usize) -> impl Iterator<Item = usize> {
+    gen move {
+        loop {
+            n = next(n);
+            yield n;
+        }
+    }
+}
+
+fn price_changes(prices: &[usize]) -> impl Iterator<Item = isize> {
+    prices
+        .iter()
+        .copied()
+        .tuple_windows()
+        .map(|(a, b)| b as isize - a as isize)
 }
 
 fn next(n: usize) -> usize {
